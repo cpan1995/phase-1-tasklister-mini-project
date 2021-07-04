f4d484d6db0fd61bc065050fe18e90349cac9b7b
cpan1995 <cpan1995@gmail.com>
2021-07-04 17:47:00 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("#create-task-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    let newTask = document.createElement('LI');
+    newTask.textContent = e.target[0].value; 
+    let newButton = document.createElement('BUTTON')
+    console.log(newButton)
+    newButton.setAttribute("data-description", e.target[0].value)
+    newButton.setAttribute("id", 'taskButton');
+    newButton.textContent = 'X'
+
+    newButton.addEventListener('click', () => {
+      console.log(document.querySelectorAll('li'));
+      document.querySelectorAll('li').forEach(element => {
+        element.remove();
+      })
+      document.querySelectorAll('#taskButton').forEach(element => {
+        element.remove()
+      })
+    })
+    
+    newTask.contentEditable = true;
+    //newTask.appendChild(newButton)
+    document.querySelector("ul").append(newTask, newButton);
+    document.querySelector("#create-task-form").reset();
+
+  })
+
+  
 });
